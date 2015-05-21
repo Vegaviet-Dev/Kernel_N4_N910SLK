f3d07eedcc4384e947c1e0260e2e19924c36e57b
sadven <mattitude2010@gmail.com>
2015-05-21 14:26:15 +1200

    FIOPS and Rel C
    Conflicts:
    	arch/arm/configs/meisie_defconfig
    	drivers/cpufreq/cpufreq_interactive.c~
    	include/config/kernel.release
    	include/config/tristate.conf
    	include/generated/compile.h
    	include/generated/utsrelease.h

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,14 @@
-/* This file is auto generated, version 3 */
+/* This file is auto generated, version 6 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
+<<<<<<< HEAD
 #define UTS_VERSION "#3 SMP PREEMPT Sun May 17 20:26:55 NZST 2015"
 #define LINUX_COMPILE_BY "matt"
 #define LINUX_COMPILE_HOST "matt-VirtualBox"
 #define LINUX_COMPILER "gcc version 4.9.3 20150515 (prerelease) (UBERTC-4.9.3) "
+=======
+#define UTS_VERSION "#6 SMP PREEMPT Thu May 21 14:02:51 NZST 2015"
+#define LINUX_COMPILE_BY "mattdev"
+#define LINUX_COMPILE_HOST "mattdev-ubu64"
+#define LINUX_COMPILER "gcc version 4.8.4 20141110 (prerelease) (crosstool-NG linaro-1.13.1+bzr2709 - Linaro GCC 2014.11) "
+>>>>>>> 8eb5b8d... FIOPS and Rel C
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 #define UTS_RELEASE "3.10.9MeiKernUberer0.5+"
+=======
+#define UTS_RELEASE "3.10.9-gcaaddaa-dirty"
+>>>>>>> 8eb5b8d... FIOPS and Rel C
